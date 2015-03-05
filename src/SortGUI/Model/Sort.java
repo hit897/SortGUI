0b0c583ee504092757df0d941175ed36df68519e
hit897 <Hit897@gmail.com>
2015-03-05 12:56:25 -0700

    g
    g

--- src/SortGUI/Model/Sort.java
+++ src/SortGUI/Model/Sort.java
@@ -2,5 +2,67 @@ package SortGUI.Model;
 
 public class Sort
 {
-
+	
+	
+	private long startTime;
+	private long endTime;
+	private long sortTime;
+	
+	public long getSortTime()
+	{
+		return endTime;
+		
+	}
+	
+	public String sortingTime(long sortTime)
+	{
+		String timeToSort = "";
+		
+		timeToSort += "Days: " + sortTime/(1000*60*60*24) + "/n";
+		timeToSort += "Hours: " + sortTime/(1000*60*60*24) + "/n";
+		timeToSort += "Minutes: " + sortTime/(1000*60*60*24) + "/n";
+		timeToSort += "Seconds: " + sortTime/(1000*60*60*24) + "/n";
+		timeToSort += "Milliseconds: " + sortTime/(1000*60*60*24) + "/n";
+		
+		return timeToSort;
+	}
+	
+	public int[] selectionSort(int [] toBeSorted)
+	{
+		int minimum;
+		int minimumPosition;
+		startTime= System.currentTimeMillis();
+		for(int position = 0; position < toBeSorted.length; position++)
+		{
+			minimumPosition = position;
+			minimum = toBeSorted[position];
+			for(int next = position +1; next < toBeSorted.length; next++)
+			{
+				if(toBeSorted[next] < minimum)
+				{
+					minimum = toBeSorted[next];
+					minimumPosition = next;
+				}
+				if(minimumPosition != position)
+				{
+					swap(toBeSorted, position, minimumPosition);
+				}
+			}
+			endTime = System.currentTimeMillis();
+			
+			sortTime = endTime - startTime;
+			
+			return toBeSorted;
+		}
+			
+		
+		return toBeSorted;
+	}
+	
+	private void swap(int[] array, int position, int change)
+	{
+		int temp = array[position];
+		array[position] = array[change];
+		array[change] = temp;
+	}
 }
